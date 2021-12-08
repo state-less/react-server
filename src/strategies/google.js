7ace469900a4b632cda2580b7b571b8296ea9c9c
Moritz Roessler <moritz.roessler@gmail.com>
2021-12-08 14:03:35 +0100

    fix: add google publickey

--- src/strategies/google.js
+++ src/strategies/google.js
@@ -1,11 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const pem = `-----BEGIN RSA PUBLIC KEY-----
-MIGJAoGBAKyFZt4dVhm7yo5r866cDxXsgFiHj91vO8/gvZNGGq3jyBdGO+yoGjAf
-N3Fb+R6fOQoXvmf52/8tJAYazoWuP6v+oBRDocs/D1GUs3WNs5IV/A3Ivr4nPR7p
-S+XBVowxj18HpBy5vxJl5D5Cru/up25MZsHFUDBl9pRHmsAqqjZpAgMBAAE=
------END RSA PUBLIC KEY-----`;
-
+const pem = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ…cQvdi82UgJIPjg/ZbMt\nUwIDAQAB\n-----END PUBLIC KEY-----\n"
 
 export const recover = (challenge, response) => {
     const token = jwt.verify(response, pem);
