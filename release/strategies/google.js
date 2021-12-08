971f3791987f931cbc2a53e8bdd29df6d7f0bda1
Moritz Roessler <moritz.roessler@gmail.com>
2021-12-08 14:08:26 +0100

    fix: add google publickey

--- release/strategies/google.js
+++ release/strategies/google.js
@@ -10,6 +10,7 @@ const jwt = require('jsonwebtoken');
 const pem = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ…cQvdi82UgJIPjg/ZbMt\nUwIDAQAB\n-----END PUBLIC KEY-----\n";
 
 const recover = (challenge, response) => {
+  console.log("Verifiying", response, pem);
   const token = jwt.verify(response, pem);
   return token;
 };
