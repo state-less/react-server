379183ac2f6c2dfe0743699f7cea20e59e650759
Moritz Roessler <moritz.roessler@gmail.com>
2022-02-04 04:02:13 +0100

    fix: use correcto store constructor

--- jsx-renderer/jsx-runtime.js
+++ jsx-renderer/jsx-runtime.js
@@ -2,12 +2,10 @@ const logger = require('../src/lib/logger');
 
 
 const jsxs = (Component, props, key) => {
-    logger.error`JSX RUNTINE ${props} ${key}`
     const {store, ...rest} = props;
     return Component(props, key, {store});
 }
 const jsx = (Component, props, key) => {
-    logger.error`JSX RUNTINE ${props} ${key}`
     const {store, ...rest} = props;
     const component = Component(props, key, {store});
     component.key = key;
@@ -16,7 +14,6 @@ const jsx = (Component, props, key) => {
 }
 
 const Fragment = (args) => {
-    logger.error`JSX RUNTINE`
     return {}
 }
 module.exports = {jsx, jsxs, Fragment};
