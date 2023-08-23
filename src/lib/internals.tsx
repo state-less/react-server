b1cba465927b11b1f9907202078d1740a62ba38e
Moritz Roessler <morrossl@publicisgroupe.net>
2023-08-23 11:27:06 +0200

    fix: add parent key to render

--- src/lib/internals.tsx
+++ src/lib/internals.tsx
@@ -127,7 +127,12 @@ export const render = <T,>(
     isClientContext(requestContext) &&
     JSON.stringify(rendered) !== JSON.stringify(renderCache[key])
   ) {
-    console.log('DIFF', rendered, renderCache[key]);
+    console.log(
+      'DIFF',
+      JSON.stringify(rendered),
+      JSON.stringify(renderCache[key]),
+      parent?.key
+    );
     console.log(`Rerendering component ${key}`);
     Dispatcher.getCurrent()._pubsub.publish(
       generateComponentPubSubKey(tree, requestContext as ClientContext),
