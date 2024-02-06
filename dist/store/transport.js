978241d9b95c87e761631a180b7a2f4c2935ff47
Moritz Roessler <moritz.roessler@gmail.com>
2024-02-06 23:03:45 +0100

    wip: add useQuery

--- dist/store/transport.js
+++ dist/store/transport.js
@@ -258,7 +258,7 @@ var PostgresTransport = /*#__PURE__*/function (_Transport) {
               where = ['user', 'key', 'client', 'scope', 'id'].filter(function (k) {
                 return stateOptions[k];
               }).map(function (k, i) {
-                return "".concat(k, " = $").concat(i);
+                return "".concat(k, " = $").concat(i + 1);
               }).join(' AND ');
               query = "SELECT * FROM states WHERE ".concat(where);
               console.log('QUERY', query);
